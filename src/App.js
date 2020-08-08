30d0ddaa054065066049be63ebc9ad04b911e66b
trinhdinhhiep <63467833+trinhdinhhiep@users.noreply.github.com>
2020-08-08 09:44:34 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="nen">
+      <div className="bg">
+        <p>aaaaaaaaaaaaaaa</p>
+        <p>aaaaaaaaaaaaaaa</p>
+        <p>aaaaaaaaaaaaaaa</p>
+        <p>aaaaaaaaaaaaaaa</p>
+      </div>
+    </div>
+  );
+}
